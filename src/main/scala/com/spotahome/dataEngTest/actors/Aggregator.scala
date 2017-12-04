0c0b00f996fac89c173b172857bf78348e0691a7
Sotos Matzanas <smatzanas@homeaway.com>
2017-12-04 23:01:31 +0100

    Ebugeed works ok but dirty

--- src/main/scala/com/spotahome/dataEngTest/actors/Aggregator.scala
+++ src/main/scala/com/spotahome/dataEngTest/actors/Aggregator.scala
@@ -14,7 +14,6 @@ import akka.util.Timeout
 import com.spotahome.dataEngTest.TwitterTrends.AskPartialsSignal
 import com.spotahome.dataEngTest.actors.Aggregator.PartialProcessed
 import com.spotahome.dataEngTest.actors.PartialAggregator.RegisterWithAggregator
-import com.spotahome.dataEngTest.utils.Utilities
 
 object Aggregator {
 
@@ -28,29 +27,32 @@ class Aggregator extends Actor {
   implicit val timeout = Timeout(200 milliseconds)
 
   private val partialAggregators = mutable.HashSet[ActorRef]()
-  private val previousResults = mutable.Set[(String, Int)]()
+  private val previousResults = ArrayBuffer[(String, Int)]()
 
   private def futureToFutureTry[T](f: Future[T]): Future[Try[T]] = {
       f.map(Success(_)).recover({case e => Failure(e)})
   }
 
-  def zipSeqWithIndex(s: Iterable[(String, Int)]): Map[String, Int] = (for (e <- s) yield e._1).zipWithIndex.toMap
+  def zipSeqWithIndex(s: Iterable[(String, Int)]): Seq[(String, Int)] = {
+    s.map(t => t._1).zipWithIndex.toSeq.sortBy(_._2)
+  }
 
-  private def coalesceResults(currentResults : Iterable[(String, Int)]): immutable.Iterable[(String, Int, String)] = {
-    val previousIndices = zipSeqWithIndex(previousResults)
+  def coalesceResults(currentResults : Iterable[(String, Int)]): Seq[(String, Int, String)] = {
+    val previousIndices = zipSeqWithIndex(previousResults).toMap
     previousResults.clear()
     previousResults ++= currentResults
-    val j = currentResults.toMap
-    for ( (k, i) <- zipSeqWithIndex(currentResults)) yield {
+
+    val j: Map[String, Int] = currentResults.map(t => t._1 -> t._2).toMap
+    (for ( (k, i) <- zipSeqWithIndex(currentResults)) yield {
       previousIndices.get(k) match {
-        case Some(index) if index < i => (k, j.get(k).get, s"\u2193 (${Math.abs(i - index)} positions")
-        case Some(index) if index > i => (k, j.get(k).get, s"\u2191 (${Math.abs(i - index)} positions")
+        case Some(index) if index < i => (k, j.get(k).get, s"\u2193 (${Math.abs(i - index)} positions)")
+        case Some(index) if index > i => (k, j.get(k).get, s"\u2191 (${Math.abs(i - index)} positions)")
         case Some(_)  => (k, j.get(k).get, s"=")
         case None => (k, j.get(k).get, "new")
       }
-    }
-
+    }).sortBy(- _._2)
   }
+
 
   override def receive = {
     case RegisterWithAggregator() =>
@@ -67,9 +69,9 @@ class Aggregator extends Actor {
         case _ => {
           println(s"current ${currentResults}")
           println(s"Is this my total? ${currentResults.sortBy(- _._2).take(10)}")
-          //val res = coalesceResults(Utilities.convertMapToSortedSeqByValue(currentResults.toMap).take(10))
+          val res = coalesceResults(currentResults.sortBy(- _._2).take(10))
           println("COALESCED")
-          //res.foreach(println)
+          res.foreach(println)
         }
       })
 
--- src/main/scala/com/spotahome/dataEngTest/utils/Utilities.scala
+++ src/main/scala/com/spotahome/dataEngTest/utils/Utilities.scala
@@ -1,6 +1,7 @@
 package com.spotahome.dataEngTest.utils
 
-import scala.collection.mutable.TreeMap
+import scala.collection.{immutable, mutable}
+import scala.collection.mutable.{ArrayBuffer, TreeMap}
 
 object Utilities extends App {
 
@@ -18,7 +19,52 @@ object Utilities extends App {
     //foo.keys
   }
 
-  convertMapToSortedSeqByValue( Map("#justinbieber" -> 1, "#mtvhottest" -> 14,
-    "#other" -> 1, "#records" -> 1, "#retweet" -> 1, "#top50nomino" -> 1, "#vinyl" -> 1))
+  /*convertMapToSortedSeqByValue( Map("#justinbieber" -> 1, "#mtvhottest" -> 14,
+    "#other" -> 1, "#records" -> 1, "#retweet" -> 1, "#top50nomino" -> 1, "#vinyl" -> 1))*/
 
+  private val previousResults = ArrayBuffer[(String, Int)]()
+
+  def zipSeqWithIndex(s: Iterable[(String, Int)]): Seq[(String, Int)] = {
+    s.map(t => t._1).zipWithIndex.toSeq.sortBy(_._2)
+  }
+
+  def coalesceResults(currentResults : Iterable[(String, Int)]): Seq[(String, Int, String)] = {
+    val previousIndices = zipSeqWithIndex(previousResults).toMap
+    previousResults.clear()
+    previousResults ++= currentResults
+
+    val j: Map[String, Int] = currentResults.map(t => t._1 -> t._2).toMap
+    (for ( (k, i) <- zipSeqWithIndex(currentResults)) yield {
+      println(s"key ${k} new index: ${i}, old index was ${previousIndices.get(k)}")
+      previousIndices.get(k) match {
+        case Some(index) if index < i => (k, j.get(k).get, s"\u2193 (${Math.abs(i - index)} positions)")
+        case Some(index) if index > i => (k, j.get(k).get, s"\u2191 (${Math.abs(i - index)} positions)")
+        case Some(_)  => (k, j.get(k).get, s"=")
+        case None => (k, j.get(k).get, "new")
+      }
+    }).sortBy(- _._2)
+  }
+
+  /*var currentResults = ArrayBuffer[(String, Int)]("#justinbieber" -> 1, "#mtvhottest" -> 14,
+    "#other" -> 1, "#records" -> 1, "#retweet" -> 1, "#top50nomino" -> 1, "#vinyl" -> 1)
+  val r1 = coalesceResults(currentResults.sortBy(- _._2))
+  println("----A----")
+  r1.foreach(println)
+  currentResults = ArrayBuffer[(String, Int)]("#justinbieber" -> 5, "#mtvhottest" -> 14,
+    "#other" -> 1, "#records" -> 3, "#retweet" -> 1, "#top50nomino" -> 6, "#vinyl" -> 1)
+  val r2 = coalesceResults(currentResults.sortBy(- _._2))
+  println("----b----")
+  r2.foreach(println)*/
+
+
+  var cr1 = ArrayBuffer(("#starwars",3), ("#losultimosjedi",2), ("#swipe4thenextsong",1), ("#mtvhottest",1),
+    ("#electronicarts",1), ("#lucasfilm",1), ("#jedi",1), ("#offroad",1), ("#gmc",1), ("#daisyridley...",1))
+  val r3 = coalesceResults(cr1.sortBy(- _._2))
+  println("----A----")
+  r3.foreach(println)
+  val cr4 = ArrayBuffer(("#mtvhottest",5), ("#starwars",3), ("#scifi",1), ("#madrid",1),
+    ("#daisyridley...",1), ("#losultimosjedi",1), ("#jedi",1))
+  val r4 = coalesceResults(cr4.sortBy(- _._2))
+  println("----A----")
+  r4.foreach(println)
 }
